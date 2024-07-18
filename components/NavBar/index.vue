19c5ba50775e7252dc7323b1660d51e0f3542833
Diego Parra <diegomiguelp1996@gmail.com>
2024-07-18 15:52:46 +0200

    Update navbar

--- components/NavBar/index.vue
+++ components/NavBar/index.vue
@@ -3,39 +3,51 @@ const navElements = [
   {
     name: "Analize",
     path: "/resume/analize",
-    icon: "heroicons:magnifying-glass",
+    icon: "bx:id-card",
   },
   {
     name: "Build",
     path: "/resume/build",
-    icon: "request-quote",
+    icon: "bx:pencil",
   },
   {
     name: "History",
     path: "/resume/history",
-    icon: "iconamoon:history-bold",
+    icon: "bx:history",
   },
   {
     name: "Github",
     path: "https://github.com/0bCdian/Waypaper-Engine",
-    icon: "uil:github",
+    icon: "line-md:github",
   },
 ];
 </script>
 
 <template>
-  <header>
-    <nav class="flex justify-between">
-      <div class="ml-2"><NuxtLink to="/">CvSpark</NuxtLink></div>
-      <ul class="flex space-x-4 mr-2">
+  <header class="bg-white">
+    <nav class="flex items-center justify-between h-14">
+      <div class="ml-4">
+        <NuxtLink to="/"
+          ><span
+            class="font-extrabold border-black border-2 p-1 bg-[#F7CB45] hover:bg-[#F7CB45] hover:shadow-[2px_2px_0px_rgba(0,0,0,1)] active:bg-[#00E1EF] rounded-sm"
+            >CVSPARK.</span
+          ></NuxtLink
+        >
+      </div>
+      <ul class="flex space-x-4 mr-4">
         <li
           v-for="({ name, path, icon }, index) in navElements"
           :key="index"
-          class=""
+          class="border-black border-2 p-1 bg-[#A6FAFF] hover:bg-[#79F7FF] hover:shadow-[2px_2px_0px_rgba(0,0,0,1)] active:bg-[#00E1EF] rounded-sm"
         >
-          <NuxtLink :to="path">
-            <Icon :name="icon" />
-            {{ name }}
+          <NuxtLink
+            :to="path"
+            norel
+            :target="name === 'Github' ? '_blank' : '_self'"
+            class="flex space-x-1 justify-between items-center"
+          >
+            <Icon :name="icon" size="14" color="black" />
+            <span class="text-xs font-semibold">{{ name }}</span>
           </NuxtLink>
         </li>
       </ul>
